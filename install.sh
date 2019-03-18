1cbd0f33d5bfa8589f40072c6d5f93a157858bd3
peternimbus <peter@nimbus-ap.co.uk>
2019-03-18 14:06:52 +0000

    Added command to ensure .profile is loaded on .bash_profile

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 #git clone https://github.com/psillifant/publicdotfiles ~/
 ln -s ~/publicdotfiles/.profile ~/.profile
 ln -s ~/publicdotfiles/.vimrc ~/.vimrc
+
+[[ -f "~/.bash_profile" ]] && echo ". ~/.profile" >> ~/.bash_profile
