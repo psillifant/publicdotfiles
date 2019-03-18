64873009763849fe4304aeabbd651050d58b41b1
peternimbus <peter@nimbus-ap.co.uk>
2019-03-18 18:57:55 +0000

    Updated script to move to the /Users/peter/publicdotfiles directory and back

--- install.sh
+++ install.sh
@@ -12,7 +12,11 @@ if [[ "`echo $BASH_VERSION | cut -b 1`" -lt "4" ]]; then
     exit 1
 elif [[ "$#" -gt "0" ]]; then
     if [[ "${1,,}" = "update" ]]; then
-        git fetch "$HOME/publicdotfiles" && git pull "$HOME/publicdotfiles"
+        currdir="${pwd}"
+        cd "$HOME/publicdotfiles"
+        git fetch
+        git pull
+        cd ${currdir}
         if [[ "$?" != "0" ]]; then
             echo "Unable to update git repo at: $HOME/publicdotfiles, despite request to do so.  Exiting..."
             exit 2
@@ -38,6 +42,7 @@ UpdateLink(){
         echo "${target_file} exists, and is not linked correctly to ${source_file}.  Taking no action"
         return 1
     else
+        echo "${target_file} is not setup correctly.  Linking to ${source_file}"
         ln -s "${source_file}" "${target_file}"
     fi
 }
